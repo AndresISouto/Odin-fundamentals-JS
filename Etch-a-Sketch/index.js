db1888c0fe57492ed78b7a6a1362dc30ab301024
AndrseISouto <186386252+AndresISouto@users.noreply.github.com>
2025-01-27 09:34:47 +0100

    add color and oppacity effects

--- Etch-a-Sketch/index.js
+++ Etch-a-Sketch/index.js
@@ -3,19 +3,30 @@ const boton = document.querySelector("button");
 //  (\(\
 //  ( -.-)  
 //  o_(")(")
+
 //  (\(\
 //  ( -.-)  
 //  o_(")(")
 function grid(number){
     number = parseInt(number);
     // const ancho = Math.floor(100/number);
-    ancho = 100/number;
+    let ancho = 100/number;
     number = number**2;
+    let color1,color2,color3;
     for (let i = 0; i < number; i++){
+        let opacity = 1.0;
         const caja = document.createElement("div");
+        caja.style.width = `${ancho}%`;
+        caja.style.height = `${ancho}%`;
+        color1 = Math.floor(Math.random()*256);
+        color2 = Math.floor(Math.random()*256);
+        color3 = Math.floor(Math.random()*256);
+        caja.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
         caja.classList.toggle("caja");
-        caja.style.width = `${ancho}%`
-        caja.style.height = `${ancho}%`
+        caja.addEventListener("mousemove",()=>{
+            opacity -= 0.1;
+            caja.style.opacity = `${opacity}`;
+        })
         contenedor.appendChild(caja); 
     }
 
@@ -30,4 +41,6 @@ boton.addEventListener("click",()=>{
     grid(numero);
 })
 
+/* Toca ver como bajar la opacidad al hacer hover sobre las casillas, haria falta un hover 
+o algo asi */
 
